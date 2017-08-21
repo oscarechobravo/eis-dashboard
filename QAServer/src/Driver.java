5ba17318c0a796d548a0ee1c4510f41a3e1fb129
Ross Newby <rossnewby@hotmail.co.uk>
2017-08-21 15:47:27 +0100

    Add files via upload

---
+++ QAServer/src/Driver.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Ross on 21/08/2017.
+ */
+public class Driver {
+
+    public Driver(){
+
+    }
+
+    public static void main (String args []){
+
+        System.out.println("Running...");
+        new Driver();
+    }
+}
+
+
