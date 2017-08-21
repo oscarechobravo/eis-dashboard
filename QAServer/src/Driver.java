856cb9d4f1411f93e65e1116d8527764382db5a5
Ross Newby <rossnewby@hotmail.co.uk>
2017-08-21 17:55:30 +0100

    HTTP Request*
    *Error: 'PKIX path building failed' = Problem with CKAN certificate. Followed steps @ link below, but still no luck, might be my machine's jre; will investigate:
    
    https://stackoverflow.com/questions/21076179/pkix-path-building-failed-and-unable-to-find-valid-certification-path-to-requ

--- QAServer/src/Driver.java
+++ QAServer/src/Driver.java
@@ -1,10 +1,48 @@
+import sun.net.www.http.HttpClient;
+import sun.net.www.protocol.http.HttpURLConnection;
+
+import javax.net.ssl.HttpsURLConnection;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
 /**
- * Created by Ross on 21/08/2017.
+ * @Author Ross Newby
  */
 public class Driver {
 
-    public Driver(){
+    private final String USER_AGENT = "Mozilla/5.0";
 
+    private String cookie = "cosign-https-ckan.lancaster.ac.uk";
+    private String cookiename = "VdV48bc+gJh8i4NRtvZx95OTVkyGCtoaF-TSaIOm7lz-9v3kxvvlqodDKiUKwvaNpMiuLWkgerUGySpMETKd6SqtyU9bKpcZ6pS5bydnVKAcgdUsHWlDA1dWNJ2l/1503307766";
+
+    public Driver(){
+        try {
+            // http://www.baeldung.com/java-http-request
+            URL url = new URL("https://ckan.lancaster.ac.uk/api/3/action/package_show?id=planonmetadata");
+            HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("Cookie", cookiename+"="+cookie); // cookie header for access
+
+            // read response
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(con.getInputStream())); // TODO: PKIX path building fails, problem with certificate
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            System.out.println(response.toString()); // for now just print
+
+            // close connections
+            in.close();
+            con.disconnect();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     public static void main (String args []){
@@ -13,5 +51,8 @@ public class Driver {
         new Driver();
     }
 }
+
+
+
 
 
